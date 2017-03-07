661e899f447917ded88c93d142caddb025ffd6fd
Rober Oltra Vercher <Rober Oltra Vercher>
2017-03-07 13:29:48 +0100

    Tercera versión DOS ACTIVIDADES COMPLETADAS En esta version, tenemos una pantalla principal con un menu con el cual podemos pasar a otra pantalla (activity) y en esta elegir uno de los 3 colores.

--- TresActividades/app/src/main/java/com/example/rober/tresactividades/Activity2.java
+++ TresActividades/app/src/main/java/com/example/rober/tresactividades/Activity2.java
@@ -1,13 +1,179 @@
 package com.example.rober.tresactividades;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
+
+    private TextView colorR, colorG, colorB;
+
+    private final Activity THIS = this;
+
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        //vincular el boton del layaut al de programa
+        colorR = (TextView) findViewById(R.id.colorR);
+        colorG = (TextView) findViewById(R.id.colorG);
+        colorB = (TextView) findViewById(R.id.colorB);
+
+        //escuchador del boton
+        colorR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //cuando pulse
+            public void onClick(View v) {
+                Toast.makeText(THIS, "ROJO_pulsado", Toast.LENGTH_LONG).show();
+
+                //---------que salga el dialogo de Si,No,Cancelar
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                // set title
+                alertDialogBuilder.setTitle("Opción pulsada");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("¿Seguro que quieres escoger el ROJO?")
+                        .setCancelable(false)
+                        .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //cuando le doy a si
+                                Toast.makeText(THIS, "ROJO_elegido", Toast.LENGTH_LONG).show();
+                                //hacer invisible el resto
+                                colorG.setVisibility(TextView.INVISIBLE);
+                                colorB.setVisibility(TextView.INVISIBLE);
+
+                                dialog.cancel();
+
+
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //cuando le doy a no
+                                Toast.makeText(THIS, "Vuelve a elegir", Toast.LENGTH_LONG).show();
+                                dialog.cancel();
+                            }
+                        });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+
+            }
+        });
+
+        //escuchador del boton
+        colorG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //cuando pulse
+            public void onClick(View v) {
+                Toast.makeText(THIS, "VERDE_pulsado", Toast.LENGTH_LONG).show();
+                //---------que salga el dialogo de Si,No,Cancelar
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                // set title
+                alertDialogBuilder.setTitle("Opción pulsada");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("¿Seguro que quieres escoger el VERDE?")
+                        .setCancelable(false)
+                        .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //cuando le doy a si
+                                Toast.makeText(THIS, "VERDE_elegido", Toast.LENGTH_LONG).show();
+                                //hacer invisible el resto
+                                colorR.setVisibility(TextView.INVISIBLE);
+                                colorB.setVisibility(TextView.INVISIBLE);
+
+                                dialog.cancel();
+
+
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //cuando le doy a no
+                                Toast.makeText(THIS, "Vuelve a elegir", Toast.LENGTH_LONG).show();
+                                dialog.cancel();
+                            }
+                        });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+
+            }
+        });
+
+        //escuchador del boton
+        colorB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //cuando pulse
+            public void onClick(View v) {
+                Toast.makeText(THIS, "AZUL_pulsado", Toast.LENGTH_LONG).show();
+                //---------que salga el dialogo de Si,No,Cancelar
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                // set title
+                alertDialogBuilder.setTitle("Opción pulsada");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("¿Seguro que quieres escoger el AZUL?")
+                        .setCancelable(false)
+                        .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //cuando le doy a si
+                                Toast.makeText(THIS, "AZUL_elegido", Toast.LENGTH_LONG).show();
+                                //hacer invisible el resto
+                                colorG.setVisibility(TextView.INVISIBLE);
+                                colorR.setVisibility(TextView.INVISIBLE);
+
+                                dialog.cancel();
+
+
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //cuando le doy a no
+                                Toast.makeText(THIS, "Vuelve a elegir", Toast.LENGTH_LONG).show();
+                                dialog.cancel();
+                            }
+                        });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+
+            }
+        });
+
+
     }
+
+
 }
